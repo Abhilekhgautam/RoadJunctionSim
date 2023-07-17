673a7d45667bd7192657534513074733426ec5e0
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-07-18 00:33:55 +0545

    bug fix: check light last updated befor staus update, add lights

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#define OLC_SOUNDWAVE
+#include "olcSoundWaveEngine.h"
 #include <chrono>
+#include <algorithm>
 
 class Car{
 public:
@@ -78,11 +81,22 @@ public:
     RoadSim() {
         sAppName = "Simulation of Road Junction";
     }
+
+    bool outOfScreen(Car& car){
+        if(car.getX() < -10 || car.getY() < -10 || car.getX() > ScreenWidth() + 30 || car.getY() > ScreenHeight()) return true;
+        else return false;
+    }
+
     bool OnUserCreate() override {
 
         sprHorizontal = std::make_unique<olc::Sprite>("../sprites/horizontal_road.png");
         sprVertical = std::make_unique<olc::Sprite>("../sprites/vertical_road.png");
         sprJunction = std::make_unique<olc::Sprite>("../sprites/junction.png");
+        sprPond = std::make_unique<olc::Sprite>("../sprites/pond.png");
+
+        bgmEngine.InitialiseAudio();
+        // background music
+        bgm = std::make_unique<olc::sound::Wave>("../music/carmoving.wav");
 
         vCar.emplace_back((float)13 * 15, (float) 9 * 32, 0,15, "../sprites/car-red-bottom-top.png");
         vCar.emplace_back((float)0, (float) 4 * 33, 13,0, "../sprites/car-red-left-right.png");
@@ -91,16 +105,43 @@ public:
         vCar.emplace_back((float)13 * 32, (float) 4 * 37, -15,0, "../sprites/car-yellow-right-left.png");
 
         m_StartTime = std::chrono::steady_clock::now();
+        moveX = false;
+        moveY = true;
+
+        bgmEngine.PlayWaveform(bgm.get(), true);
+
         return true;
     }
     bool OnUserUpdate(float fElapsedTime) override{
-        Clear(olc::BLACK);
-
-        int curr_dur = (int)elapsedSeconds();
+        Clear(olc::CYAN);
         drawRoad();
 
+        int curr_dur = (int)elapsedSeconds();
+
         for(auto& elt: vCar){
-            DrawSprite(olc::vi2d((int)elt.getX(), (int)elt.getY()), elt.getPtr());
+            if(!outOfScreen(elt))
+             DrawSprite(olc::vi2d((int)elt.getX(), (int)elt.getY()), elt.getPtr());
+        }
+
+        //Draw pond
+        DrawSprite(olc::vi2d((int)300, 200), sprPond.get());
+        DrawSprite(olc::vi2d((int)10, 160), sprPond.get());
+        DrawSprite(olc::vi2d((int)10, 60), sprPond.get());
+
+
+        // Draw Strip(for Traffic Light)
+        for(int i = 0 ; i < 60 ; i++){
+            if(moveX){
+                FillCircle( i * 8,  4 * 32 + 15,1, olc::GREEN);
+                if(i >= 16 && i <= 20) continue;
+                FillCircle(6 * 32 + 15, i * 8, 1, olc::RED);
+            }
+
+            if(moveY) {
+                FillCircle(i * 8, 4 * 32 + 15, 1, olc::RED);
+                if(i >= 16 && i <= 20) continue;
+                FillCircle(6 * 32 + 15, i * 8, 1, olc::GREEN);
+            }
         }
 
         // Update Car Position..
@@ -111,8 +152,9 @@ public:
         // display time..
         DrawString(0,0,"Timer:" + std::to_string(curr_dur) + " Sec");
 
-        if(curr_dur % 4 == 0 && curr_dur != last_manufactured) {
+        if(curr_dur % 5 == 0 && curr_dur != last_manufactured) {
                 vCar.emplace_back(myFactory.BottomToTop());
+                if(curr_dur % 10 == 0 )
                 vCar.emplace_back(myFactory.TopToBottom());
                 vCar.emplace_back(myFactory.LeftToRight());
                 vCar.emplace_back(myFactory.RightToLeft());
@@ -120,10 +162,14 @@ public:
             last_manufactured = curr_dur;
         }
 
+        bgmEngine.PlayWaveform(bgm.get(), false);
+
         // update traffic light
-        if(curr_dur != 0 && curr_dur % 40 == 0){
+        if(curr_dur != 0 && curr_dur % 20 == 0 && last_updated !=curr_dur){
             moveX = !moveX;
             moveY = !moveY;
+
+            last_updated = curr_dur;
         }
 
         return true;
@@ -142,22 +188,44 @@ public:
             DrawSprite(olc::vi2d((int)  6 * 32, (int)  i * 32), sprVertical.get());
         }
         //draw horizontal portion of the road
-        for(int i = 7 ; i < 14 ; i++){
+        for(int i = 7 ; i < 15 ; i++){
             DrawSprite(olc::vi2d((int)  i * 32, (int)  4 * 32), sprHorizontal.get());
         }
     }
 
     float getJunDist(Car& car) const {
         if(getMoveDirection(car) == 0)
-            return (abs(car.getY() - 3.50f * 32.0f));
+            return (car.getY() - 3.50f * 32.0f);
             // bottom to top
         else if(getMoveDirection(car) == 2)
-            return(abs(car.getY() - 5.0f * 32.0f));
+            return(car.getY() - 5.0f * 32.0f);
             // right to left
         else if(getMoveDirection(car) == 1)
-            return(abs(car.getX() - 7.20f * 32.0f));
+            return(car.getX() - 7 * 32.0f);
         //left to right
-        else return (abs(car.getX() - 5.30f * 32.0f));
+        else return (car.getX() - 5 * 32.0f);
+    }
+
+    bool crossedJun(Car& car){
+        if(getMoveDirection(car) == 0) {
+            if ((car.getY() - 3.50f * 32.0f) < 0) return false;
+            return true;
+        }
+            // bottom to top
+        else if(getMoveDirection(car) == 2) {
+                if ((car.getY() - 5.0f * 32.0f) > 0) return false;
+                return true;
+            }
+            // right to left
+        else if(getMoveDirection(car) == 1){
+            if((car.getX() - 7 * 32.0f) > 0) return false;
+            return true;
+        }
+            //left to right
+        else {
+            if((car.getX() - 6 * 32.0f) < 0) return false;
+            return true;
+        }
     }
 
     int getMoveDirection(Car& car) const{
@@ -171,43 +239,67 @@ public:
         else return 3;
     }
 
-    bool shouldStopJun(Car& car) const {
-        int dir = getMoveDirection(car);
-        if(moveX){
-            if((dir == 1 || dir == 3)) return false;
-            else if (getJunDist(car) > 0) return false;
-            else return true;
+    // fix this :(
+    bool shouldStopJun(Car& car) {
+        auto dir = getMoveDirection(car);
+        if((dir == 0 || dir == 2) && moveX){
+            if(crossedJun(car)) return false;
+            else {
+                if(dir == 0){
+                    if(abs(car.getY() + 4 - 3.50f * 32.0f) > 0 && abs(car.getY() - 3.50f * 32.0f) <= 3) return true;
+                    else return false;
+                }
+                else{
+                    if(abs(car.getY() + 4- 5 * 32.0f) > 0 && abs(car.getY()  - 5 * 32.0f) <= 3) return true;
+                    else return false;
+                }
+            }
         }
-        else if(moveY){
-            if((dir == 0 || dir == 2)) return false;
-            else if (getJunDist(car) > 0) return false;
-            else return true;
+        else if((dir == 1 || dir == 3) && moveY){
+            if(crossedJun(car)) return false;
+            else {
+                if(dir == 1){
+                    if(abs(car.getX()  - 7 * 32.0f) > 0 && abs(car.getX() - 7 * 32.0f) <= 3) return true;
+                    else return false;
+                }
+                else{
+                    if(abs(car.getX() - 5.5f * 32.0f) > 0 && abs(car.getX() - 5.5f * 32.0f) <= 3) return true;
+                    else return false;
+                }
+            }
         }
-        else return true;
+        else if((dir == 1 || dir == 3) && moveX){
+           return false;
+        }
+        else if((dir == 0 || dir == 2) && moveY){
+            return false;
+        }
+        else return false;
     }
 
+    // same lane collision prevention
     bool shouldStopCollision(Car& car)  {
           for (auto &elt: vCar) {
             if (getMoveDirection(car) == getMoveDirection(elt) && elt != car) {
                 int dir = getMoveDirection(car);
                 if(dir == 0){
                     if(car.getY() < elt.getY()){
-                        if(abs(car.getY() - elt.getY()) < 25) return true;
+                        if(std::abs(car.getY() - elt.getY()) < 30) return true;
                     }
                 }
                 else if(dir == 1){
                     if(car.getX() > elt.getX()){
-                        if(abs(car.getX() - elt.getX()) < 25) return true;
+                        if(std::abs(car.getX() - elt.getX()) < 30) return true;
                     }
                 }
                 else if(dir == 2){
                     if(car.getY() > elt.getY()){
-                        if(abs(car.getY() - elt.getY()) < 25) return true;
+                        if(std::abs(car.getY() - elt.getY()) < 30) return true;
                     }
                 }
                 else{
                     if(car.getX() < elt.getX()){
-                        if(abs(car.getX() - elt.getX()) < 25) return true;
+                        if(std::abs(car.getX() - elt.getX()) < 30) return true;
                     }
                 }
             }
@@ -219,6 +311,12 @@ private:
     std::unique_ptr<olc::Sprite> sprHorizontal;
     std::unique_ptr<olc::Sprite> sprVertical;
     std::unique_ptr<olc::Sprite> sprJunction;
+
+    std::unique_ptr<olc::Sprite> sprPond;
+    std::unique_ptr<olc::Sprite> sprGarden;
+
+    olc::sound::WaveEngine bgmEngine;
+    std::unique_ptr<olc::sound::Wave> bgm;
 
     bool moveX = true;
     bool moveY = false;
@@ -234,6 +332,7 @@ private:
 
     std::chrono::time_point<std::chrono::steady_clock> m_StartTime;
     int last_manufactured = 0;
+    int last_updated = 0;
 
     double elapsedSeconds(){
         std::chrono::time_point<std::chrono::steady_clock> endTime;
